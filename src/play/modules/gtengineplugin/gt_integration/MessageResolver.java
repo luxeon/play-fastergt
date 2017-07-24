e7af5aa81eb9a440c8416165a8c08adfb0774949
dshvechikov <Dmytro.Shvechikov@gbtec.de>
2017-07-24 17:21:11 +0300

    improve message resolver api

--- src/play/modules/gtengineplugin/gt_integration/MessageResolver.java
+++ src/play/modules/gtengineplugin/gt_integration/MessageResolver.java
@@ -1,7 +1,15 @@
 package play.modules.gtengineplugin.gt_integration;
 
+import java.util.Locale;
+
 public interface MessageResolver {
 
-    String resolve(Object key, Object[] args);
+    String resolve(Object key, Object... args);
+
+    String resolveMessage(Locale locale, Object key, Object... args);
+
+    boolean supportsNativeMessages(Locale locale);
+
+    String translate(String text, Locale sourceLocale, Locale targetLocale);
 
 }
